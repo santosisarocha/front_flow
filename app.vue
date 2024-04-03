47ca869d71b101c64010d059f6349cb1b90792a6
Vinicius Cavalieri <125322132+ViniBOYs@users.noreply.github.com>
2024-04-02 22:14:35 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <iframe
+    allow="microphone;"
+    width="350"
+    height="430"
+    src="https://console.dialogflow.com/api-client/demo/embedded/a53089e3-0741-4fd7-ba6c-a7a21975b62e">
+</iframe>
+      
+  </div>
+</template>
